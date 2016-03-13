03fa31a18efc80ad5c15a90c0485a2b377249ac6
mahedi <mahedi_h@yahoo.com>
2016-03-13 02:57:01 +0000

    have to learn angular.js

---
+++ chapter_3_Variables/test1.py
@@ -0,0 +1,10 @@
+proverb = "A penny saved"
+proverb = proverb + " is a penny earned"
+print(proverb)
+
+pennies_saved = 0
+pennies_saved = pennies_saved + 1
+
+print(pennies_saved)
+
+print(pennies_saved + 1)
